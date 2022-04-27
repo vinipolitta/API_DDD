b72b374019987731edf5b3dba17b9ad449140a76
vinipolitta <vinicius.politta1@gmail.com>
2022-04-27 06:45:39 -0300

    CRIACAO API_DDD

---
+++ API_DDD/API_DDD/Entidades/Noticia.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Entidades
+{
+    [Table("TB_NOTICIA")]
+
+    public class Noticia
+    {
+        [Column("NTC_ID")]
+        public int Id { get; set; }
+
+        [Column("NTC_TITULO")]
+        [MaxLength(255)]
+        public string Titulo { get; set; }
+
+        [Column("NTC_INFORMACAO")]
+        [MaxLength(255)]
+        public string Informacao { get; set; }
+
+        [Column("NTC_ATIVO")]
+        public bool Ativo { get; set; }
+
+        [Column("NTC_DATA_CADASTRO")]
+        public DateTime DataCadstro { get; set; }
+
+        [Column("NTC_DATA_ALTERACAO")]
+        public DateTime DataAlteracao { get; set; }
+    }
+}
---
+++ API_DDD/API_DDD/Notificacoes/Notifica.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Notificacoes
+{
+    public class Notifica
+    {
+        public Notifica()
+        {
+            Notificacoes = new List<Notifica>();
+        }
+
+        [NotMapped]
+        public string NomePropriedade { get; set; }
+
+        [NotMapped]
+        public string Mensagem { get; set; }
+
+        [NotMapped]
+        public List<Notifica> Notificacoes;
+
+        public bool ValidarPropriedadeDecimal(decimal valor, string nomePropriedade)
+        {
+            if (valor < 1 || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notificacoes.Add(new Notifica
+                {
+                    Mensagem = "Valor deve ser maior que 0",
+                    NomePropriedade = nomePropriedade
+                });
+                return false;
+            }
+            return true;
+        }
+        public bool ValidarPropriedadeString(string valor, string nomePropriedade)
+        {
+            if (string.IsNullOrWhiteSpace(nomePropriedade) || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notificacoes.Add(new Notifica
+                {
+                    Mensagem = "Campo Obrigatorio",
+                    NomePropriedade = nomePropriedade
+                });
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
