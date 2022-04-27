fb0c01944a83de9d20ffbb2c3121b55e6022222b
vinipolitta <vinicius.politta1@gmail.com>
2022-04-27 09:27:31 -0300

    CRIAÇÃO DA CAMADA APLICAÇÃO

---
+++ API_DDD/Aplicacao/Aplicacoes/AplicacaoNoticia.cs
@@ -0,0 +1,63 @@
+﻿using Aplicacao.Interfaces;
+using Dominio.Interfaces;
+using Dominio.Interfaces.InterfaceServicos;
+using Entidades.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacao.Aplicacoes
+{
+    public class AplicacaoNoticia : IAplicacaoNoticia
+    {
+        INoticia _INoticia;
+        IServicoNoticia _IServicoNoticia;
+        public AplicacaoNoticia(INoticia INoticia, IServicoNoticia IServicoNoticia)
+        {
+            _INoticia = INoticia;
+            _IServicoNoticia = IServicoNoticia;
+        }
+        public async Task AdicionaNoticia(Noticia noticia)
+        {
+            await _IServicoNoticia.AdicionaNoticia(noticia);
+        }
+
+        public async Task AtualizaNoticia(Noticia noticia)
+        {
+            await _IServicoNoticia.AtualizaNoticia(noticia);
+        }
+
+        public async Task<List<Noticia>> ListarNoticiasAtivas()
+        {
+           return await _IServicoNoticia.ListarNoticiasAtivas();
+        }
+
+        public async Task Adicionar(Noticia obj)
+        {
+            await _INoticia.Adicionar(obj);
+        }
+
+        public async Task Atualizar(Noticia obj)
+        {
+            await _INoticia.Atualizar(obj);
+        }
+
+        public async Task<Noticia> BurscarPorId(int Id)
+        {
+            return await _INoticia.BurscarPorId(Id);
+        }
+
+        public async Task Excluir(Noticia obj)
+        {
+            await _INoticia.Excluir(obj);
+        }
+
+        public async Task<List<Noticia>> Listar()
+        {
+            return await _INoticia.Listar();
+        }
+
+    }
+}
---
+++ API_DDD/Aplicacao/Interfaces/Genericos/IGenericaAplicacao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacao.Interfaces.Genericos
+{
+    public interface IGenericaAplicacao<T> where T : class
+    {
+        Task Adicionar(T obj);
+        Task Atualizar(T obj);
+        Task Excluir(T obj);
+        Task<T> BurscarPorId(int Id);
+        Task<List<T>> Listar();
+    }
+}
